67017340a6bbd5d0131ad95c2d37f383898bea8e
Fabio Oliveira <fabio20rj@gmail.com>
2020-01-27 17:39:34 -0300

    feat: added tabular in lines

--- stock/admin.py
+++ stock/admin.py
@@ -2,8 +2,13 @@ from django.contrib import admin
 from .models import Stock, StockItems
 
 
+class StockItemsInLine(admin.TabularInline):
+    model = StockItems
+    extra = 0
+
 @admin.register(Stock)
 class StockAdmin(admin.ModelAdmin):
+    inlines = (StockItemsInLine,)
     list_display = ('__str__', 'invoice')
     search_fields = ('invoice',)
     list_filter = ('employ',)
