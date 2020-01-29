8938b00449606f64a7e69472d32693cbb4657818
Fabio Oliveira <fabio20rj@gmail.com>
2020-01-28 23:16:38 -0300

    feat: added test to product_list view

--- product/tests.py
+++ product/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
-# Create your tests here.
+class ProductListTest(TestCase):
+    def setUp(self):
+        self.response = self.client.get('/produtos/')
+
+    def test_get(self):
+        """Must return status code 200"""
+        self.assertAlmostEqual(200, self.response.status_code)
+
+    def test_template(self):
+        """Must use template product_list.html"""
+        self.assertTemplateUsed(self.response, 'product_list.html')
